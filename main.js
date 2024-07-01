45ff311216ecede538cc77b3861fc274c80beafd
Facundo Bisso <facundobisso5@gmail.com>
2024-07-01 20:50:51 -0300

    tercera preentrega js

--- main.js
+++ main.js
@@ -1,36 +1,92 @@
+let botonCompra = document.querySelectorAll(".botoncompra");
+const cuerpo = document.getElementById("cuerpo");
 
-const albumesPF = [{ title: "The Piper at the Gates of Dawn", price: 8000 }, { title: "A Momentary Lapse of Reason", price: 8500 }, { title: "A Saucerful of Secrets", price: 9000 }, { title: "More", price: 7500 }, { title: "Ummagumma", price: 9500 }, { title: "Atom Heart Mother", price: 9500 }, { title: "Meddle", price: 9500 }, { title: "Obscured by Clouds", price: 9500 }, { title: "The Dark Side of the Moon", price: 9500 }, { title: "Wish You Were Here", price: 9500 }, { title: "Animals", price: 9500 }, { title: "The Wall", price: 9500 }, { title: "The Final Cut", price: 9500 }, { title: "A Momentary Lapse of Reason", price: 9500 }, { title: "The Division Bell", price: 9500 }, { title: "The Endless River", price: 9500 }]
+const getRandomId = () => {
+    return Math.floor(Math.random() * Date.now()).toString(16);
+};
 
-function comprarAlbum(album) {
-    if (!album) {
-        alert("Discos disponibles de Pink Floyd: 1. The Piper at the Gates of Dawn, 2. A Momentary Lapse of Reason, 3. A Saucerful of Secrets, 4. More, 5. Ummagumma, 6. Atom Heart Mother, 7. Meddle, 8. Obscured by Clouds, 9. The Dark Side of the Moon, 10. Wish You Were Here, 11. Animals, 12. The Wall, 13. The Final Cut, 14. A Momentary Lapse of Reason, 15. The Division Bell, 16. The Endless River.")
-        let compra = prompt("Ingresa el nombre de un album de Pink Floyd que desees comprar.");
-        encontrarAlbum(compra)
-    }
-    let albumSeleccionado = prompt("¿Usted desea comprar " + album.title + "?" + " Su valor sera de " + album.price + " pesos argentinos.");
-    if (albumSeleccionado.toLowerCase().trim() === "no" || !albumSeleccionado.trim()) {
-        const confirmacionCompra1 = "Su compra fue rechazada."
-        alert(confirmacionCompra1)
-    }
-    if (albumSeleccionado.toLowerCase().trim() === "si") {
-        const confirmacionCompra = "¡Su compra fue un exito!";
-        alert(confirmacionCompra);
-    }
-}
-comprarAlbum()
+const createTask = (taskId, taskNombre, taskEmail, taskTarjeta, taskDireccion) => {
+    return {
+        id: taskId,
+        nombre: taskNombre,
+        email: taskEmail,
+        tarjeta: taskTarjeta,
+        direccion: taskDireccion
+    };
+};
 
-function encontrarAlbum(compra) {
-    let encontrado = false;
-    for (let i = 0; i < albumesPF.length; i++) {
-        if (albumesPF[i].title.toLowerCase() === compra.toLowerCase().trim()) {
-            console.log("Album " + albumesPF[i].title + " encontrado")
-            encontrado = true;
-            comprarAlbum(albumesPF[i]);
-            break;
+botonCompra.forEach(boton => {
+    boton.addEventListener('click', (e) => {
+        if (e.target.classList.contains("botoncompra")) {
+            cuerpo.remove();
+
+            let titulo = document.createElement("h1");
+            titulo.innerHTML = "El disco seleccionado ha sido añadido a su carrito.";
+            document.body.appendChild(titulo);
+
+            const formulario = document.createElement('form');
+            formulario.setAttribute('id', 'miFormulario');
+
+            const inputTexto = document.createElement('input');
+            inputTexto.setAttribute('type', 'text');
+            inputTexto.setAttribute('id', 'nombre');
+            inputTexto.setAttribute('name', 'nombre');
+            inputTexto.setAttribute('placeholder', 'Nombre');
+            formulario.appendChild(inputTexto);
+
+            const inputEmail = document.createElement('input');
+            inputEmail.setAttribute('type', 'email');
+            inputEmail.setAttribute('id', 'email');
+            inputEmail.setAttribute('name', 'email');
+            inputEmail.setAttribute('placeholder', 'Correo Electrónico');
+            formulario.appendChild(inputEmail);
+
+            const inputTarjeta = document.createElement('input');
+            inputTarjeta.setAttribute('type', 'text');
+            inputTarjeta.setAttribute('id', 'tarjeta');
+            inputTarjeta.setAttribute('name', 'tarjeta');
+            inputTarjeta.setAttribute('placeholder', 'Número de Tarjeta de Crédito');
+            formulario.appendChild(inputTarjeta);
+
+            const inputDireccion = document.createElement('input');
+            inputDireccion.setAttribute('type', 'text');
+            inputDireccion.setAttribute('id', 'direccion');
+            inputDireccion.setAttribute('name', 'direccion');
+            inputDireccion.setAttribute('placeholder', 'Dirección');
+            formulario.appendChild(inputDireccion);
+
+            const botonEnviar = document.createElement('button');
+            botonEnviar.setAttribute('type', 'submit');
+            botonEnviar.textContent = 'Enviar';
+            formulario.appendChild(botonEnviar);
+
+            formulario.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const id = getRandomId();
+                const nombre = inputTexto.value;
+                const email = inputEmail.value;
+                const tarjeta = inputTarjeta.value;
+                const direccion = inputDireccion.value;
+
+                const task = createTask(id, nombre, email, tarjeta, direccion);
+                console.log(task);
+
+                localStorage.setItem(id, JSON.stringify(task));
+
+                alert("Si ya ingresaste tus datos, la compra fue un exito.");
+
+                formulario.reset();
+            });
+
+            document.body.appendChild(formulario);
+
+            const botonRegresar = document.createElement('button');
+            botonRegresar.textContent = 'Regresar';
+            botonRegresar.addEventListener('click', () => {
+                location.reload();
+            });
+            document.body.appendChild(botonRegresar);
         }
-    }
-    if (!encontrado) {
-        alert(`El disco ${compra} no existe`);
-        comprarAlbum()
-    }
-}
+    });
+});
